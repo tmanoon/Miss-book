6416da6622568f8e330a72673c9cf6d40d13d973
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-03-11 00:04:09 +0200

    Add styling properties

--- EX/react-template-proj/cmps/BookFilter.jsx
+++ EX/react-template-proj/cmps/BookFilter.jsx
@@ -19,11 +19,11 @@ export function BookFilter({ onSetFilter, filterBy }) {
 		setFilterByToEdit((prevFilterBy) => ({ ...prevFilterBy, [field]: value }))
 	}
 
-	return <section className="book-filter">
+	return <section className="book-filter flex flex-column align-center">
 		<h2>Filter our books</h2>
 
-		<form onSubmit={onFilter}>
-			<label htmlFor="title">Title</label>
+		<form onSubmit={onFilter} className="flex align-center">
+			<label htmlFor="title">Title: </label>
 			<input type="text"
 				id="title"
 				name="title"
@@ -31,7 +31,7 @@ export function BookFilter({ onSetFilter, filterBy }) {
 				onChange={handleChange}
 				placeholder="By title" />
 
-			<label htmlFor="price">Price</label>
+			<label htmlFor="price">Price: </label>
 			<input type="number"
 				id="price"
 				name="price"
--- EX/react-template-proj/cmps/Header.jsx
+++ EX/react-template-proj/cmps/Header.jsx
@@ -8,7 +8,7 @@ export function Header({ setPage }) {
 	return <header className="app-header full">
 		<h1 className="main-header">Miss Book</h1>
 
-		<nav className="app-nav-header">
+		<nav className="app-nav-header flex justify-center">
 			<a href="" onClick={(ev) => onSetPage(ev, 'home')} >Home</a> |
 			<a href="" onClick={(ev) => onSetPage(ev, 'about')} >About</a> |
 			<a href="" onClick={(ev) => onSetPage(ev, 'books')}>Books</a>
